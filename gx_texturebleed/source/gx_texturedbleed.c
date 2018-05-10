84e1ff8e3b8e1808950e918a51ad62d5d1ee488d
booto <remornicus@gmail.com>
2018-05-10 00:23:53 -0400

    gx_texturebleed: initial import

---
+++ gx_texturebleed/source/gx_texturedbleed.c
@@ -0,0 +1,156 @@
+/* based on gxSprites demo */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <math.h>
+#include <gccore.h>
+#include <wiiuse/wpad.h>
+#include <ogc/tpl.h>
+
+#include "textures_tpl.h"
+#include "textures.h"
+
+#define DEFAULT_FIFO_SIZE	(256*1024)
+
+static void *frameBuffer[2] = { NULL, NULL};
+static GXRModeObj *rmode;
+
+
+GXTexObj texObj;
+
+//---------------------------------------------------------------------------------
+int main( int argc, char **argv ){
+//---------------------------------------------------------------------------------
+	u32	fb; 	// initial framebuffer index
+	u32 first_frame;
+	f32 yscale;
+	u32 xfbHeight;
+	Mtx44 perspective;
+	Mtx GXmodelView2D;
+	void *gp_fifo = NULL;
+
+	GXColor background = {0, 0, 0, 0xff};
+
+	VIDEO_Init();
+
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	fb = 0;
+	first_frame = 1;
+	// allocate 2 framebuffers for double buffering
+	frameBuffer[0] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	frameBuffer[1] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+
+	VIDEO_Configure(rmode);
+	VIDEO_SetNextFramebuffer(frameBuffer[fb]);
+	VIDEO_SetBlack(FALSE);
+	VIDEO_Flush();
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
+	fb ^= 1;
+
+	// setup the fifo and then init the flipper
+	gp_fifo = memalign(32,DEFAULT_FIFO_SIZE);
+	memset(gp_fifo,0,DEFAULT_FIFO_SIZE);
+
+	GX_Init(gp_fifo,DEFAULT_FIFO_SIZE);
+
+	// clears the bg to color and clears the z buffer
+	GX_SetCopyClear(background, 0x00ffffff);
+
+	// other gx setup
+	GX_SetViewport(0,0,rmode->fbWidth,rmode->efbHeight,0,1);
+	yscale = GX_GetYScaleFactor(rmode->efbHeight,rmode->xfbHeight);
+	xfbHeight = GX_SetDispCopyYScale(yscale);
+	GX_SetScissor(0,0,rmode->fbWidth,rmode->efbHeight);
+	GX_SetDispCopySrc(0,0,rmode->fbWidth,rmode->efbHeight);
+	GX_SetDispCopyDst(rmode->fbWidth,xfbHeight);
+	GX_SetCopyFilter(rmode->aa,rmode->sample_pattern,GX_TRUE,rmode->vfilter);
+	GX_SetFieldMode(rmode->field_rendering,((rmode->viHeight==2*rmode->xfbHeight)?GX_ENABLE:GX_DISABLE));
+
+	if (rmode->aa)
+		GX_SetPixelFmt(GX_PF_RGB565_Z16, GX_ZC_LINEAR);
+	else
+		GX_SetPixelFmt(GX_PF_RGB8_Z24, GX_ZC_LINEAR);
+
+
+	GX_SetCullMode(GX_CULL_NONE);
+	GX_CopyDisp(frameBuffer[fb],GX_TRUE);
+	GX_SetDispCopyGamma(GX_GM_1_0);
+
+	// setup the vertex descriptor
+	// tells the flipper to expect direct data
+	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XY, GX_F32, 0);
+	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
+
+	GX_SetNumChans(1);
+	GX_SetNumTexGens(1);
+	GX_SetTevOp(GX_TEVSTAGE0, GX_REPLACE);
+	GX_SetTevOrder(GX_TEVSTAGE0, GX_TEXCOORD0, GX_TEXMAP0, GX_COLOR0A0);
+	GX_SetTexCoordGen(GX_TEXCOORD0, GX_TG_MTX2x4, GX_TG_TEX0, GX_IDENTITY);
+
+
+	GX_InvalidateTexAll();
+
+	TPLFile alphacolTPL;
+	TPL_OpenTPLFromMemory(&alphacolTPL, (void *)textures_tpl,textures_tpl_size);
+	TPL_GetTexture(&alphacolTPL,alphacol,&texObj);
+	GX_LoadTexObj(&texObj, GX_TEXMAP0);
+
+	guOrtho(perspective,0,479,0,639,0,300);
+	GX_LoadProjectionMtx(perspective, GX_ORTHOGRAPHIC);
+
+	WPAD_Init();
+
+	while(1) {
+
+		WPAD_ScanPads();
+
+		if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) exit(0);
+
+		GX_SetViewport(0,0,rmode->fbWidth,rmode->efbHeight,0,1);
+		GX_InvVtxCache();
+		GX_InvalidateTexAll();
+
+		GX_ClearVtxDesc();
+		GX_SetVtxDesc(GX_VA_POS, GX_DIRECT);
+		GX_SetVtxDesc(GX_VA_TEX0, GX_DIRECT);
+
+		guMtxIdentity(GXmodelView2D);
+		guMtxTransApply (GXmodelView2D, GXmodelView2D, 0.0F, 0.0F, -5.0F);
+		GX_LoadPosMtxImm(GXmodelView2D,GX_PNMTX0);
+
+		GX_Begin(GX_QUADS, GX_VTXFMT0, 4);			// Draw A Quad
+			GX_Position2f32(0.0f, 0.0f);					// Top Left
+			GX_TexCoord2f32(0.0f,0.0f);
+			GX_Position2f32(rmode->fbWidth-1, 0);			// Top Right
+			GX_TexCoord2f32(1.0f,0.0f);
+			GX_Position2f32(rmode->fbWidth-1,rmode->efbHeight-1);	// Bottom Right
+			GX_TexCoord2f32(1.0f,1.0f);
+			GX_Position2f32(0,rmode->efbHeight-1);			// Bottom Left
+			GX_TexCoord2f32(0.0f,1.0f);
+		GX_End();
+
+		GX_DrawDone();
+
+		GX_SetZMode(GX_TRUE, GX_LEQUAL, GX_TRUE);
+		GX_SetBlendMode(GX_BM_BLEND, GX_BL_SRCALPHA, GX_BL_INVSRCALPHA, GX_LO_CLEAR);
+		GX_SetAlphaUpdate(GX_TRUE);
+		GX_SetColorUpdate(GX_TRUE);
+		GX_CopyDisp(frameBuffer[fb],GX_TRUE);
+
+		VIDEO_SetNextFramebuffer(frameBuffer[fb]);
+		if(first_frame) {
+			VIDEO_SetBlack(FALSE);
+			first_frame = 0;
+		}
+		VIDEO_Flush();
+		VIDEO_WaitVSync();
+		fb ^= 1;		// flip framebuffer
+	}
+	return 0;
+}
+
